3de92dd6138400d090353c9416ae180720978118
shariquefaridi7 <shariquefaridi786@gmail.com>
2024-03-17 15:28:12 +0530

    portfoilio

---
+++ src/components/Projects/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({ openModal, setOpenModal }) => {
+ 
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. From Frontend to Backend. Here are some of my projects.
+        </Desc>
+       
+        <CardContainer>
+          { projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal} />
+            ))}
+        
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
